234797870089c75864ed9455d762282c7e339270
Nap <70518391+Naffsisky@users.noreply.github.com>
2023-10-31 01:39:47 +0700

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,36 +1,59 @@
 terraform {
-        required_providers {
-                google = {
-                        source = "hashicorp/google"
-                        version = "4.51.0"
-                }
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
 }
 
 provider "google" {
-        credentials = file("service.json")
-        project = "unified-freedom-399620"
+  credentials = file("service.json")
+  project     = "unified-freedom-399620"
 }
 
-resource "google_compute_instance" "terraform" {
-        project = "unified-freedom-399620"
-        count = 3
-        name = "vm-terraform${count.index+1}"
-        machine_type = "e2-medium"
-        for_each = toset(["us-central-1", "europe-west4-a", "asia-southeast2-a"])
-        zone = each.value
-        boot_disk {
-                initialize_params {
-                        image = "debian-cloud/debian-11"
-                }
-        }
-        network_interface {
-                network = "default"
-                access_config {
-                }
-        }
+variable "zones" {
+  default = ["us-central1-c", "europe-west4-a", "asia-southeast2-a"]
+}
 
-    tags = ["http-server", "https-server", "lb-health-check"]
+resource "google_compute_instance" "server" {
+  for_each = toset(var.zones)
+  name         = "server-terraform-${each.key}"
+  machine_type = "e2-medium"
+  zone         = each.key
 
-    metadata_startup_script = "sudo apt-get update; sudo apt-get install nginx -y"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  tags                    = ["http-server", "https-server", "lb-health-check"]
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install nginx -y"
+}
+
+resource "google_compute_instance" "client" {
+  count = 1
+  name         = "client-terraform-${count.index + 1}"
+  machine_type = "e2-micro"
+  // zone         = var.zones[count.index % length(var.zones)]
+  zone         = "asia-southeast2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
 }
