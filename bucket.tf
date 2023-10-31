1243bd7c71edf2d57c5c5835c54defc58c0a1094
Nap <70518391+Naffsisky@users.noreply.github.com>
2023-10-31 18:29:31 +0700

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,3 +1,9 @@
+terraform {
+  backend "local" {
+    path = "terraform/state/terraform.tfstate"
+  }
+}
+
 provider "google" {
   project     = "qwiklabs-gcp-00-d893ae551c12"
   region      = "us-central-1"
@@ -7,10 +13,4 @@ resource "google_storage_bucket" "test-bucket-for-state" {
   location    = "US"
   uniform_bucket_level_access = true
   force_destroy = true
-}
-
-terraform {
-  backend "local" {
-    path = "terraform/state/terraform.tfstate"
-  }
 }
